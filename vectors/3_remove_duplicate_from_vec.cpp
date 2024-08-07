ca52081266423fd80e00e95009aacf788e1201cd
pramodkumarjuit <pramodkumar.juit@gmail.com>
2024-08-07 23:46:25 +0530

    Remove duplicate from unsorted vec

--- vectors/3_remove_duplicate_from_vec.cpp
+++ vectors/3_remove_duplicate_from_vec.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -17,10 +18,35 @@ void remove_duplicate(vector<int> &vec)
     vec.erase(vec.begin()+tmp.size(), vec.end());
 }
 
+// The std::unique algorithm reorders the elements so that each unique element
+// appears only once in the initial part of the range, and returns an iterator
+// to the end of the unique range.
+void removeDuplicates(std::vector<int>& vec) {
+    auto lastUnique = std::unique(vec.begin(), vec.end());
+    vec.erase(lastUnique, vec.end());
+}
+
+// works both sorted and unsorted vector
+void removeDuplicates_u(std::vector<int>& vec) {
+    // When we attempt to insert an element that already
+    // exists in the set, the set does nothing.
+    // Insertion and lookup is of O(1)
+    std::unordered_set<int> seen; // Set to track unique elements
+    std::vector<int> uniqueVec; // Vector to store unique elements
+
+    for (auto &v: vec) {
+        if(seen.find(v) == seen.end()) {
+            seen.insert(v);
+            uniqueVec.push_back(v);
+        }
+    }
+
+    vec = std::move(uniqueVec);
+}
 
 int main(int argc, char **argv) 
 {
-    vector<int> vec = {};
+    vector<int> vec = {}; // a sorted vector
     vec.push_back(1);
     vec.push_back(2);
     vec.push_back(3);
@@ -28,9 +54,10 @@ int main(int argc, char **argv)
     vec.push_back(4);
     vec.push_back(4);
     vec.push_back(5);
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
+    vec.push_back(6);
+    vec.push_back(6);
+    vec.push_back(7);
+    vec.push_back(9);
 
     cout << "Before: ";
     for (auto &e: vec) {
@@ -38,10 +65,28 @@ int main(int argc, char **argv)
     }
     cout << endl;
 
-    remove_duplicate(vec);
+    //remove_duplicate(vec);
+    removeDuplicates(vec);
 
-    cout << "After: ";
+    cout << "After:  ";
     for (auto &e: vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    ////////////////// unsorted vector //////////
+    std::vector<int> vec_u = {1, 2, 2, 3, 4, 4, 5, 5, 5};
+
+    cout << "Unsorted vector Before: ";
+    for (auto &e: vec_u) {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    removeDuplicates_u(vec_u);
+
+    cout << "Unsorted vector After:  ";
+    for (auto &e: vec_u) {
         cout << e << " ";
     }
     cout << endl;
